858f15cb6fed80e42a934808ce729de630495dfe
Graham Watson <graham@phantomwatson.com>
2013-10-02 13:06:44 -0400

    Remove unneeded flash() method

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -21,13 +21,4 @@ class AppController extends Controller {
 			));	
 		}
 	}
-
-	// Adds a string message with a class of 'success', 'error', or 'notification' (default)
-	// OR adds a variable to dump and the class 'dump' 
-	public function flash($message, $class = 'notification') {
-		// Dot notation doesn't seem to allow for the equivalent of $messages['error'][] = $message	
-		$stored_messages = $this->Session->read('FlashMessage');
-		$stored_messages[] = compact('message', 'class');
-		$this->Session->write('FlashMessage', $stored_messages);
-	}
 }
